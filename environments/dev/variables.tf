73f94e49e5f1c0c3694ebd11a56b509e0889654e
ashrujitpal <ashrujitpal1@gmail.com>
2023-04-02 19:28:55 +0530

    Changed the mail.tf file under environment

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,8 +1,32 @@
-variable "app_name" {}
-variable "app_environment" {}
-variable "app_domain" {}
-variable "app_node_count" {}
-variable "gcp_region_1" {}
-variable "gcp_zone_1" {}
-variable "private_subnet_cidr_1" {}
-variable "app_project" {}
+variable "app_name" {
+    type = "string"
+    default = "ashrujit-gcp"
+}
+variable "app_environment" {
+    type = "string"
+    default = "dev"
+}
+variable "app_domain" {
+    type = "string"
+    default = "ashrujit-gcp.com" 
+}
+variable "app_node_count" {
+    type = "int"
+    default = 2 
+}
+variable "gcp_region_1" {
+    type = "string"
+    default = "europe-west1" 
+}
+variable "gcp_zone_1" {
+    type = "string"
+    default = "europe-west1-b" 
+}
+variable "private_subnet_cidr_1" {
+    type = "string"
+    default = "10.10.1.0/24" 
+}
+variable "app_project" {
+    type = "string"
+    default = "blue-green-mig" 
+}
