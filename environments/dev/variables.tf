44e3ec2efbaa4427e9b4061dc20879c94c097986
ashrujitpal <ashrujitpal1@gmail.com>
2023-04-02 18:56:38 +0530

    Changed the mail.tf file under environment

---
+++ environments/dev/variables.tf
@@ -0,0 +1,8 @@
+variable "app_name" {}
+variable "app_environment" {}
+variable "app_domain" {}
+variable "app_node_count" {}
+variable "gcp_region_1" {}
+variable "gcp_zone_1" {}
+variable "private_subnet_cidr_1" {}
+variable "app_project" {}
